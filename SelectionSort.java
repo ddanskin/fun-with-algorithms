aa813d52f482d06eae18f0ff5216d820c5bbc3b7
Danielle Danskin <danielle.danskin@gmail.com>
2018-06-10 11:46:05 -0400

    create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+/* ----------------------------------------------------------------------------
+*	By: Danielle Danskin
+* 	Created: 06/10/2018
+* 	Last updated: 06/10/2018
+*
+*	Compilation: javac SelectionSort.java
+* 	Execution: java SelectionSort
+*
+*	This program sorts an array using selection sort.
+* ---------------------------------------------------------------------------*/
+
+public class SectionSort {
+	public SelectionSort(arr1) {
+	}
+
+	public static void main(String[] args) {
+	
+	In in = new In(args[0]);
+	}
+}
